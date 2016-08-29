b15f82bbea245e87aafa4cfaa3bcb63bf81a6a0b
Yong <lomanyong@gmail.com>
2016-08-30 00:45:04 +0800

    remove unused dependencies

--- WePeiYang/app/src/main/java/com/twt/service/support/StatusBarHelper.java
+++ WePeiYang/app/src/main/java/com/twt/service/support/StatusBarHelper.java
@@ -1,20 +1,13 @@
 package com.twt.service.support;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Build;
-import android.view.WindowManager;
-
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-import com.twt.service.R;
 
 
 /**
  * Created by Rex on 2015/8/1.
  */
 public class StatusBarHelper {
-    private static com.readystatesoftware.systembartint.SystemBarTintManager tintManager;
 
     public static void setColor(Activity activity, int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
