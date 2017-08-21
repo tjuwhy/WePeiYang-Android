411607f591cb4a0f1485d5e07d141948e5ceff6e
liuyuesen <1301913704@qq.com>
2017-08-21 20:50:10 +0800

    fenhaolebao

--- WePeiYang/repair/src/main/java/com/twtstudio/repair/main/MainActivity.java
+++ WePeiYang/repair/src/main/java/com/twtstudio/repair/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.twtstudio.repair;
+package com.twtstudio.repair.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.twtstudio.repair.R;
 
 public class MainActivity extends AppCompatActivity {
 
