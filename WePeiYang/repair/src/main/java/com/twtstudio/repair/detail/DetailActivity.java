8a004e720ad6ea01fcd48c586e92164a8d692294
tjwhm <759361109@qq.com>
2017-08-23 08:39:51 +0800

    wrote BaseActivity

--- WePeiYang/repair/src/main/java/com/twtstudio/repair/detail/DetailActivity.java
+++ WePeiYang/repair/src/main/java/com/twtstudio/repair/detail/DetailActivity.java
@@ -1,11 +1,42 @@
 package com.twtstudio.repair.detail;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+
+import com.twtstudio.repair.R;
+import com.twtstudio.repair.commons.BaseActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by tjwhm on 2017/8/22 8:04.
  * Happy coding!
  **/
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends BaseActivity implements DetailCotract.DetailPresenter {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_detail;
+    }
+
+    @Override
+    protected Toolbar getToolbarView() {
+        toolbar.setTitle("报修详情");
+        return toolbar;
+    }
+
+    @Override
+    protected boolean isShowBackArrow() {
+        return true;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
---
+++ WePeiYang/repair/src/main/java/com/twtstudio/repair/detail/DetailCotract.java
@@ -0,0 +1,15 @@
+package com.twtstudio.repair.detail;
+
+/**
+ * Created by tjwhm on 2017/8/23 8:25.
+ * Happy coding!
+ **/
+
+public interface DetailCotract {
+    public interface DetailView{
+
+    }
+    public interface DetailPresenter{
+
+    }
+}
