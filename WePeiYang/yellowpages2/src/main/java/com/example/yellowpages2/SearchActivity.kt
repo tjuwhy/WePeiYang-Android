a120c275ae15f4aba3cf021bab4c9e9c85bf0432
HaoYuWang <2481101267@qq.com>
2018-09-06 18:44:22 +0800

    emmm开始写SearchActivity。

--- WePeiYang/yellowpages2/src/main/java/com/example/yellowpages2/SearchActivity.kt
+++ WePeiYang/yellowpages2/src/main/java/com/example/yellowpages2/SearchActivity.kt
@@ -2,11 +2,26 @@ package com.example.yellowpages2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
+import android.view.Menu
+import android.view.MenuItem
+import scut.carson_ho.searchview.SearchView
 
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var mSearchView:SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+//        searchView = findViewById(R.id.yellow_page_search_view)
+//        searchView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_view,menu)
+        val searchItem = menu?.findItem(R.id.menu_search)
+        mSearchView = MenuItemCompat.getActionView(searchItem) as SearchView
+        return super.onCreateOptionsMenu(menu)
     }
 }
